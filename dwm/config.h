ccd3381296ff48a2babf7ef28f77c5ae4bb61537
gaiusgaius <91840524+gaiusgaius@users.noreply.github.com>
2022-04-01 11:06:57 +0200

    Update config.h

--- dwm/config.h
+++ dwm/config.h
@@ -39,7 +39,7 @@ static const Rule rules[] = {
 /* window swallowing */
 static const int swaldecay = 3;
 static const int swalretroactive = 1;
-static const char swalsymbol[] = "👅";
+static const char swalsymbol[] = "�";
 
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
